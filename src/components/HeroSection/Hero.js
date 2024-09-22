d09022c4207459e7c2a9c09e2e5dc71f88fa857c
dachman <dachman@dachman.dachman>
2024-09-22 16:40:02 +0530

    NavBar is done

---
+++ src/components/HeroSection/Hero.js
@@ -0,0 +1,12 @@
+import "./Hero.css"
+
+
+const Hero = () => {
+   return (
+       <div>
+         Hero Section
+       </div>       
+  )
+}
+
+export default Hero
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,28 @@
 import React from 'react'
-import "./NavBar"
+import "./NavBar.css"
+import logo from "../../images/icon.svg"
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='nav-bar'>
+    <div>
+    <img  className="logo" src={logo} alt="image not found"/>
+    </div>
+    
+      <div className='url-list'>
+          <ul>
+             <li> 
+              <a>Home</a>
+            </li>
+             <li>
+              <a>About</a>
+              </li>
+             <li>
+               <a>Contact</a>
+            </li>
+          </ul>
+      </div>     
+        
+    </div>
   )
 }
