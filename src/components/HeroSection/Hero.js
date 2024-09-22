8322631fa4c33a91aa360d5d15da5ee647c8b489
dachman <dachman@dachman.dachman>
2024-09-22 17:00:51 +0530

    Navbar Template

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -3,8 +3,13 @@ import "./Hero.css"
 
 const Hero = () => {
    return (
-       <div>
-         Hero Section
+       <div id="Hero" className="hero-container">
+           <p>
+             We bring Customer for your brands<br/> and Products using Artifical <br/>Intelligence
+           </p>
+           <a className="contact-btn">
+              Contact
+           </a>
        </div>       
   )
 }
