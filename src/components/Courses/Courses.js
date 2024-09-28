bef11eb85be43011e1c02ee0cc015b3d8a058632
dachman <dachman@dachman.dachman>
2024-09-28 21:24:55 +0530

    Courses Section is Done

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import "./courses.css"
+import "./Courses.css"
 
 const Courses = () => {
   return (
     <div className='course-container'>
-         <h2>Courses</h2> 
-         <div className='container-details'>
-             <div className='box'>
-
-             </div>
-             <div className='box'>
-
-             </div>
+         <div className='sub-container'>
+             <h2 className='title'>Courses</h2>
+          <div className='box-container'>
+               <div className='box'>
+                   <h2>Want to Learn AI and latest tech?</h2>
+                   <button>
+                        Check Courses
+                   </button>
+               </div>
+               <div className='box'>
+                    <h2>Want to educate underprivileged kids</h2>
+                    <button>Support</button>
+               </div>
+          </div>
+             
+            
          </div>
-
+        
     </div>
   )
 }
