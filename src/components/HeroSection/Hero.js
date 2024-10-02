4fb84a9cd26875afb9b5f7988d91248eb447f11a
dachman <dachman@dachman.dachman>
2024-10-02 13:30:56 +0530

    minor changers

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -53,7 +53,7 @@ const Hero = () => {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            width: 300,
+            width: 350,
             bgcolor: 'background.paper',
             boxShadow: 24,
             p: 4,
